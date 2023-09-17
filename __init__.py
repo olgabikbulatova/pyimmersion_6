b027e08079881e628eade5dd69b54ee1d151c993
Olga B <orlett@yandex.ru>
2023-09-17 22:47:09 +0300

    add new func in init file

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .task6_2 import guess_number
 from .task6_3 import puzzle, show_res
 from .hw6_1 import data_check
+from .hw6_2 import qween_check, qween_lst
 
-__all__ = ['guess_number', 'puzzle', 'show_res', 'data_check']
+__all__ = ['guess_number', 'puzzle', 'show_res', 'data_check', 'qween_check', 'qween_lst' ]
